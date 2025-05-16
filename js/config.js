07724032fb6f01e09ccf66d043edb59e0dc4aab4
GDNJerome <111050910+GDNJerome@users.noreply.github.com>
2025-05-16 14:37:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_IEwVLlc5m', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7vdgiam1em85h6p8gri8gj6hkp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
